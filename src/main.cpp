028ce6da725d0efeed77cf026b7586e946c72264
Elienay Nathan <elienayngc@gmail.com>
2020-11-03 16:19:11 -0300

    Incluindo o cabecalho number

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include "number.hpp"
+
 using namespace std;
 
 int main () {
+    Number number;
 
+    cout << "Escreva um número: ";
+    cin >> number.get_value();
+
+    system("pause > nul");
     
     return 0;
 }
